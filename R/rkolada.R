aad9486fc61f7d5380e23f1c51efb7258ec14b4f
Thomas Reinholdsson <reinholdsson@gmail.com>
2015-04-20 13:45:26 +0200

    Minor fixes

--- R/rkolada.R
+++ R/rkolada.R
@@ -17,18 +17,22 @@ Kolada <- R6Class("Kolada",
       return(res)
     },
     
-    fetch_meta_data = function(x) {
-      D <- self$fetch('http://api.kolada.se/v2/%s', x)
+    fetch_meta_data = function(x, y = NULL) {
+      url <- if (is.null(y)) {
+        'http://api.kolada.se/v2/%s'
+      } else 'http://api.kolada.se/v2/%s/%s'
+      
+      D <- self$fetch(url, x, pasteC(y))
       setnames(D, names(D), paste(x, names(D), sep = '.'))
       return(D)
     },
     
-    kpi = function() {
-      self$fetch_meta_data('kpi')
+    kpi = function(...) {
+      self$fetch_meta_data('kpi', ...)
     },
     
-    municipality = function() {
-      self$fetch_meta_data('municipality')
+    municipality = function(...) {
+      self$fetch_meta_data('municipality', ...)
     },
     
     values = function(kpi, municipality, year, all = F) {
---
+++ README.md
@@ -0,0 +1,22 @@
+# rkolada
+
+    # devtools::install_github('reinholdsson/rkolada')
+    library(rkolada)
+    x <- rkolada()
+
+## Get kpi:s
+
+    x$kpi()  # all
+    x$kpi('N00008')
+    x$kpi(c('N00008', 'N00009))
+
+## Get municipalities
+
+    x$municipality()  # all
+    x$municipality('1440')
+    x$municipality(1440:1489)
+
+## Get values
+
+    x$values('N00006', '0764', 2000:2010)
+    
